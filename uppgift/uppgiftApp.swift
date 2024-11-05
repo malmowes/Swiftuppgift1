1c9afda46b9cbde9cb47c1e25ff02338006ba791
wes <malmowes@hotmail.com>
2024-11-05 17:13:34 +0100

    Initial Commit

---
+++ uppgift/uppgiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  uppgiftApp.swift
+//  uppgift
+//
+//  Created by applwes on 2024-11-05.
+//
+
+import SwiftUI
+
+@main
+struct uppgiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ uppgiftTests/uppgiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  uppgiftTests.swift
+//  uppgiftTests
+//
+//  Created by applwes on 2024-11-05.
+//
+
+import Testing
+@testable import uppgift
+
+struct uppgiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
