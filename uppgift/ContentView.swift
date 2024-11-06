195624b8d8a40f8f2a51e281900bfe037a0ac2bc
wes <malmowes@hotmail.com>
2024-11-06 20:04:32 +0100

    fix

--- uppgift/ContentView.swift
+++ uppgift/ContentView.swift
@@ -5,17 +5,70 @@
 //  Created by applwes on 2024-11-05.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            
+            HStack(spacing: 0.0){
+                VStack{}
+                    .frame(maxWidth: .infinity, maxHeight: 100)
+                    .background(Color.red)
+                VStack{}
+                    .frame(maxWidth: .infinity, maxHeight: 100)
+                    .background(Color.green)
+                VStack{}
+                    .frame(maxWidth: .infinity, maxHeight: 100)
+                    .background(Color.yellow)
+            }
+            VStack{
+                
+                
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: 100)
+            .background(Color.gray)
+            
+            VStack{}
+                .frame(width: 100, height: 100)
+                .background(Color.white)
+            
+           
+            Spacer()
+            
+       
+            HStack{
+                Spacer()
+                VStack{}
+                    .frame(width: 80, height: 80)
+                    .background(Color.black)
+                    .padding()
+                
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: 100)
+            .background(Color.red)
+            
+
+            
+            
+            
+            
+            
+            
+            
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.cyan)
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
